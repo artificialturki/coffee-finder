474615fe169a3aee4e9e66409bb2e61041510aab
artificialturki <artificial.turki@gmail.com>
2025-09-11 03:12:22 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("coffee-finder-v1").then(cache => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/app.js", "/manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
